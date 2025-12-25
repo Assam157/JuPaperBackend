c8a4d41ec7300403b3f9e1eb5a447f8278ff0409
Assam157 <asasm8106@gmail.com>
2025-12-25 17:49:13 +0530

    Update MongoDB.js

--- Backend/MongoDB/MongoDB.js
+++ Backend/MongoDB/MongoDB.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-    "mongodb+srv://Maitreya:killdill12@cluster0.sk6ugig.mongodb.net/PaperModelJU?retryWrites=true&w=majority&tls=true",
+    process.env.mongourl,
 )
 .then(() => console.log("Connected to MongoDB!"))
 
 .catch(err => console.error("Connection error:", err));
+
